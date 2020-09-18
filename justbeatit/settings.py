aa868ad9069f5a5d903458ffd44da0998c6c5108
HISSANDANISH <hissandanish@gmail.com>
2020-09-18 22:57:40 +0530

    Added databases conn

--- justbeatit/settings.py
+++ justbeatit/settings.py
@@ -93,10 +93,8 @@ DATABASES = {
        'PORT' :'5432'
     }
 }
-#DATABASES = {
-#   'default':
-#       dj_database_url.config()
-#}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
